45f595ead125e480ce4034cee9df23742d89257d
accipiter117 <matthewadamhawkes@gmail.com>
2026-09-26 18:54:05 +0100

    Route new decisions

--- tests/tournament.sim.js
+++ tests/tournament.sim.js
@@ -29,7 +29,7 @@ function seats(bySeat, fallback) {
   const route = name => (state, f, ...rest) => pick(f)[name](state, f, ...rest);
   const methods = ['chooseStormDial', 'chooseTraitor', 'choosePrediction', 'chooseBid', 'chooseRevival',
     'chooseShipmentAndMovement', 'chooseBattlePlan', 'choosePrescienceElement', 'chooseVoice', 'chooseCardsToDiscard',
-    'chooseCaptureAction', 'chooseWormRide', 'chooseRevealTraitor', 'chooseBreakAlliance', 'chooseAllianceProposal', 'chooseAllianceResponse'];
+    'chooseCaptureAction', 'chooseWormRide', 'chooseRevealTraitor', 'chooseBreakAlliance', 'chooseAllianceProposal', 'chooseAllianceResponse', 'chooseTruthtrance', 'chooseKaramaCancel', 'chooseAllyPledge', 'chooseEmperorAllyRevival', 'chooseDiscards'];
   return Object.fromEntries(methods.map(m => [m, route(m)]));
 }
 
